50b84e73a4e809719fa7c9aedaec60180ecd0538
NickKartha <nickkartha@gmail.com>
2018-06-25 23:56:21 +0530

    Hello World
    To raise interest on the backtick ( ` ) and asterisk ( * )

---
+++ hello.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# This is a comment!
+
+echo "Hello      World"
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World        #all names in the folder
+echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo `hello` world        #won't find hello
+echo 'hello' world
+
+#try it out to see the fun
